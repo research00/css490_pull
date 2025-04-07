33a663ecdedccfa40b58994c4860876b33b50a86
Viktor Schultz <viktor.schultz00@gmail.com>
2025-04-07 13:51:57 -0700

    merge successfull

--- Conflict.java
+++ Conflict.java
@@ -1,6 +1,7 @@
 public class Conflict {
     public static void main(String[] args) {
         // merge_conflict1
+        // Alec's comment to conflict with Viktor's
         System.out.println("Hello, World!");
     }
 }
