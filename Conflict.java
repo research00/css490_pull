b77d09c6bf1e3782b00181302f5047e8c1c0bf59
Viktor Schultz <viktor.schultz00@gmail.com>
2025-04-07 13:43:54 -0700

    added comment

--- Conflict.java
+++ Conflict.java
@@ -1,6 +1,6 @@
 public class Conflict {
     public static void main(String[] args) {
-        // editing this comment to create a merge conflict
+        // merge_conflict1
         System.out.println("Hello, World!");
     }
 }
